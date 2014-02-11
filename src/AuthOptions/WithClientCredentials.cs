4f20ff457d76874882d2c7bb650c530a424f8683
Yves Goeleven <yves@goeleven.com>
2014-02-11 13:23:36 +0100

    reusing access tokens across requests

--- src/AuthOptions/WithClientCredentials.cs
+++ src/AuthOptions/WithClientCredentials.cs
@@ -7,23 +7,45 @@ namespace Gateway.Rest.AcceptanceTests
 {
     public class WithClientCredentials : IAuthorize
     {
-       private const string Scope = "http://api.messagehandler.net/";
+        private const string Scope = "http://api.messagehandler.net/";
+        private static string bearer;
+        private static string refresh_token;
+        private static DateTime expires;
 
         public string GetAuthorizationHeader()
         {
             var client = new RestClient(Settings.BaseUri);
+            RestRequest request = null;
 
-            var request = new RestRequest("authorize", Method.POST);
-            request.AddParameter("client_id", Settings.ClientId);
-            request.AddParameter("client_secret", Settings.ClientSecret);
-            request.AddParameter("scope", Scope);
-            request.AddParameter("grant_type", "client_credentials");
+            if (bearer == null)
+            {
+                request = new RestRequest("authorize", Method.POST);
+                request.AddParameter("client_id", Settings.ClientId);
+                request.AddParameter("client_secret", Settings.ClientSecret);
+                request.AddParameter("scope", Scope);
+                request.AddParameter("grant_type", "client_credentials");
+            }
+            else if (expires <= DateTime.Now.AddMinutes(1))
+            {
+                request = new RestRequest("authorize", Method.POST);
+                request.AddParameter("client_id", Settings.ClientId);
+                request.AddParameter("client_secret", Settings.ClientSecret);
+                request.AddParameter("scope", Scope);
+                request.AddParameter("grant_type", "refresh_token");
+                request.AddParameter("refresh_token", refresh_token);
+            }
 
-            var response = client.Execute(request);
+            if (request != null)
+            {
+                var response = client.Execute(request);
 
-            string token = JsonConvert.DeserializeObject<dynamic>(response.Content).access_token;
+                var token = JsonConvert.DeserializeObject<dynamic>(response.Content);
 
-            return "Bearer " + Convert.ToBase64String(Encoding.UTF8.GetBytes(token));
+                bearer = "Bearer " + Convert.ToBase64String(Encoding.UTF8.GetBytes((string)token.access_token));
+                expires = DateTime.Now.AddSeconds((int)token.expires_in);
+                refresh_token = token.refresh_token;
+            }
+            return bearer;
         }
 
     }
